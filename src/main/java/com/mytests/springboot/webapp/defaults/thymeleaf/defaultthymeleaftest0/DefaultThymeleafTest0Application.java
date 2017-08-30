40b67772205b8e591ca00ee259572de8402e3565
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-08-30 15:51:15 +0300

    Initial commit

---
+++ src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest0/DefaultThymeleafTest0Application.java
@@ -0,0 +1,12 @@
+package com.mytests.springboot.webapp.defaults.thymeleaf.defaultthymeleaftest0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultThymeleafTest0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultThymeleafTest0Application.class, args);
+	}
+}
