f60f6ea9f566d8a82f61b19f5ed9d4775db79b45
irina <irina@me.com>
2017-11-24 17:58:39 +0300

    new mapping

--- src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest0/MyController.java
+++ src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest0/MyController.java
@@ -17,6 +17,14 @@ public class MyController {
     public String Method(ModelMap model) {
         model.addAttribute("home_attr1", "test0");
         model.addAttribute("home_attr2", "this is default thymeleaf configuration test");
+        model.addAttribute("next", "next_step");
         return "home";
+    }
+
+    @RequestMapping("/next_step")
+    public String Method2(ModelMap model) {
+        model.addAttribute("title","next step");
+        model.addAttribute("attr","you are here");
+        return "step2";
     }
 }
